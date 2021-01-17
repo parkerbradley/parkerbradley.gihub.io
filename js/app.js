be1b4c2e6ab0b0d82789f084fdf747b81a635f02
parkerbradley <parkerbradley93@gmail.com>
2021-01-16 23:17:54 -0600

    portfolio and scrollspy

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
 const cover = document.querySelector('.cover');
-const overlay = document.querySelector('.overlay');
 
 //----------- Helper Functions -----------//
 
 //--- finds screen height and sets as coverImg height
 function coverHeight() {
   cover.style.height = `${window.innerHeight}px`;
-  overlay.style.height = `${window.innerHeight}px`;
 }
 
 coverHeight();
+
+var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+target: '#navbar-example'
+})
